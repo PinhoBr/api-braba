b87fe82b4b872df04b6c386ceaf2d107931889dd
Pinho <143522285+PinhoBr@users.noreply.github.com>
2025-09-10 14:51:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+header('Access-Control-Allow-Origin: *');
+
+
+$usuarios = [
+    [
+        'id' => 1,
+        'nome' => 'Carlos a Silva',
+        'email' => 'carlos@exemplo.com'
+    ],
+    [
+        'id' => 2,
+        'nome' => 'Maria Oliveira',
+        'email' => 'maria@exemplo.com'
+    ],
+    [
+        'id' => 3,
+        'nome' => 'Ana Souza',
+        'email' => 'ana@exemplo.com'
+    ]
+];
+
+$json_response = json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+echo $json_response;
+
+?>
